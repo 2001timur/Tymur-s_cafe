71e9255edc0d5941acb2c23cae71a364e9b97fd9
Timur Shevchenko <timur.shevchenko7@gmail.com>
2025-07-09 22:11:51 +0300

    Add item

--- Tymur's_cafe/Program.cs
+++ Tymur's_cafe/Program.cs
@@ -1,7 +1,14 @@
-﻿namespace Tymur_s_cafe
+﻿using System.Diagnostics;
+
+namespace Tymur_s_cafe
 {
     internal class Program
     {
+
+        const decimal GST = 5;
+
+        static List<string> descriptions = new();
+        static List<decimal> prices = new();
 
         static void Menu()
         {
@@ -17,6 +24,50 @@
             Console.WriteLine("║ 7. Load from file           ║");
             Console.WriteLine("║ 0. Exit                     ║");
             Console.WriteLine("╚═════════════════════════════╝");
+
+        }
+
+        static void AddItem(string description, decimal price)
+        {
+            descriptions.Add(description);
+            prices.Add(price);
+        }
+
+        static void AddItemMenu()
+        {
+
+            if (descriptions.Count == 5)
+            {
+                Console.WriteLine("There are 5 items in the bill. This is the maximum.\n");
+                return;
+            }
+
+            string description;
+            decimal price;
+
+            Console.WriteLine();
+
+            do
+            {
+                Console.Write("Enter description: ");
+                description = Console.ReadLine();
+                if (string.IsNullOrEmpty(description) || !(description.Length >= 3 && description.Length <= 20))
+                    Console.WriteLine("Incorrect description: [3-20] symbols!");
+            } while (string.IsNullOrEmpty(description) || !(description.Length >= 3 && description.Length <= 20));
+
+
+            bool res;
+            do
+            {
+                Console.Write("Enter price: ");
+                res = decimal.TryParse(Console.ReadLine(), out price);
+                if (!res || price <= 0)
+                    Console.WriteLine("Incorrect price: more than zero $!");
+            } while (!res || price <= 0);
+
+            AddItem(description, price);
+
+            Console.WriteLine("\nAdd item was successful.\n");
 
         }
 
