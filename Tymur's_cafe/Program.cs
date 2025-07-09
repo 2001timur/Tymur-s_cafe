1a150c662a0882a3fb07cfd59e9be2aa019f61d5
Timur Shevchenko <timur.shevchenko7@gmail.com>
2025-07-09 22:10:57 +0300

    Add menu

--- Tymur's_cafe/Program.cs
+++ Tymur's_cafe/Program.cs
@@ -2,6 +2,24 @@
 {
     internal class Program
     {
+
+        static void Menu()
+        {
+            Console.WriteLine("╔═════════════════════════════╗");
+            Console.WriteLine("║        Tymur's Cafe         ║");
+            Console.WriteLine("║─────────────────────────────║");
+            Console.WriteLine("║ 1. Add Item                 ║");
+            Console.WriteLine("║ 2. Remove Item              ║");
+            Console.WriteLine("║ 3. Add Tip                  ║");
+            Console.WriteLine("║ 4. Display Bill             ║");
+            Console.WriteLine("║ 5. Clear All                ║");
+            Console.WriteLine("║ 6. Save to file             ║");
+            Console.WriteLine("║ 7. Load from file           ║");
+            Console.WriteLine("║ 0. Exit                     ║");
+            Console.WriteLine("╚═════════════════════════════╝");
+
+        }
+
         static void Main(string[] args)
         {
             
